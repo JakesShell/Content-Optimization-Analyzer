6500826b6003d6103ded123f92af4e1fb23eeb51
Jaco van Rooyen <jacovanrooyen21@gmail.com>
2024-10-19 18:43:20 +0700

    Create  App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// client/src/App.js
+import React from 'react';
+import Analyzer from './components/Analyzer';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Analyzer />
+        </div>
+    );
+};
+
+export default App;
